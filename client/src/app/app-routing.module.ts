a92c93748f412bb02d623376c1df21e7baf7e1a3
ruzny <ruzny@ruzny.com>
2020-01-27 17:49:27 +0530

    setup some components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,43 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+// COMPONENTS
+import { HomeComponent } from "./components/home/home.component";
+import { TodoComponent } from "./components/todo/todo.component";
+import { TodoItemComponent } from "./components/todo-item/todo-item.component";
+import { AboutComponent } from "./components/about/about.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+    pathMatch: "full"
+  },
+  {
+    path: "todos",
+    component: TodoComponent,
+    pathMatch: "full"
+  },
+  {
+    path: "todo/:id",
+    component: TodoItemComponent,
+    pathMatch: "full"
+  },
+  {
+    path: "about",
+    component: AboutComponent,
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const RoutingComponents = [
+  HomeComponent,
+  AboutComponent,
+  TodoComponent,
+  TodoItemComponent
+];
