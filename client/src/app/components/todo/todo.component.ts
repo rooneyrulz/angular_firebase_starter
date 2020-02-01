0106c5bcbc45f829cc83e49e4fd2a187bbb7c412
ruzny <ruzny@ruzny.com>
2020-02-01 14:20:18 +0530

    working on app

--- client/src/app/components/todo/todo.component.ts
+++ client/src/app/components/todo/todo.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+// IMPORT MODELS
+import { Todo } from "../../models/Todo";
 
 @Component({
-  selector: 'app-todo',
-  templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  selector: "app-todo",
+  templateUrl: "./todo.component.html",
+  styleUrls: ["./todo.component.css"]
 })
 export class TodoComponent implements OnInit {
+  todos: Todo[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.todos = [
+      { id: "382932", title: "todo one", body: "it is first todo item" },
+      { id: "32302930", title: "todo two", body: "it is second todo item" },
+      { id: "4738473", title: "todo three", body: "it is third todo item" },
+      { id: "463746347", title: "todo four", body: "it is fourth todo item" }
+    ];
   }
-
 }
