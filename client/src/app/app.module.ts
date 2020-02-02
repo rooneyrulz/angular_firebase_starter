2e5408686864cb176402dbd586dcff3bfb32d4af
ruzny <ruzny@ruzny.com>
2020-02-02 12:20:20 +0530

    connecting to firestore

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
 
 import { AppRoutingModule } from "./app-routing.module";
+
+// ENVIRONMENT
+import { environment } from "../environments/environment";
 
 // COMPONENTS
 import { AppComponent } from "./app.component";
@@ -9,7 +14,12 @@ import { RoutingComponents } from "./app-routing.module";
 
 @NgModule({
   declarations: [AppComponent, RoutingComponents],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(environment)
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDb4dZih_5jCRuNom3DRSmTxb0v1CMEpbE",
+    authDomain: "simple-angular-crud.firebaseapp.com",
+    databaseURL: "https://simple-angular-crud.firebaseio.com",
+    projectId: "simple-angular-crud",
+    storageBucket: "simple-angular-crud.appspot.com",
+    messagingSenderId: "265040630536",
+    appId: "1:265040630536:web:5308fdfe9364f75a0c92bd",
+    measurementId: "G-E9XWVHLZGR"
+  }
 };
 
 /*
