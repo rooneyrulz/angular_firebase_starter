3c7c0e3f0614e9a8a45c9be6b74e3c3216a516e1
ruzny <ruzny@ruzny.com>
2020-02-10 17:50:54 +0530

    added material module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AngularFireModule } from "@angular/fire";
 import { AngularFirestoreModule } from "@angular/fire/firestore";
 
 import { AppRoutingModule } from "./app-routing.module";
+import { MaterialModule } from "./material/material.module";
 
 // ENVIRONMENT
 import { environment } from "../environments/environment";
@@ -11,7 +13,6 @@ import { environment } from "../environments/environment";
 // COMPONENTS
 import { AppComponent } from "./app.component";
 import { RoutingComponents } from "./app-routing.module";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, RoutingComponents],
@@ -20,7 +21,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     AngularFirestoreModule,
     AngularFireModule.initializeApp(environment),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
